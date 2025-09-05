536cab448bb4d9fffdfdeafd78045df817c335af
Adam Zieliński <adam@adamziel.com>
2025-09-05 15:21:12 +0200

    Remove rector rules

--- rector.php
+++ rector.php
@@ -4,13 +4,26 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
+require_once __DIR__ . '/tools/RectorRules/CamelCaseToSnakeCaseVariableAndPropertyRector.php';
+
 return static function (RectorConfig $rectorConfig): void {
     // Autoload the custom Rector rule class
     $rectorConfig->autoloadPaths([
         __DIR__ . '/tools/Rector',
     ]);
 
+    // Skip certain paths from processing
+    $rectorConfig->skip([
+        __DIR__ . '/vendor/*',
+        __DIR__ . '/*/vendor-patched/*',
+        __DIR__ . '/*/Tests/*',
+    ]);
+
     // Register our custom rule
-    $rectorConfig->rule(\Tools\Rector\CamelCaseToSnakeCaseVariableAndPropertyRector::class);
+    $rectorConfig->rule(\Tools\RectorRules\CamelCaseToSnakeCaseVariableAndPropertyRector::class);
+
+    $rectorConfig->paths([
+        __DIR__ . '/components/*',
+    ]);
 };
 
