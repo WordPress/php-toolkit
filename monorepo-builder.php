61ab545111e8f5897e245f7b82bbf91ac94a5a4e
Adam Zielinski <adam@adamziel.com>
2025-09-07 01:49:07 +0200

    remove nested composer.json files to avoid changing them via monorepo-builder

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -19,13 +19,6 @@ return static function ( MBConfig $config ): void {
             __DIR__ . '/components',
         )
     );
-    // List all vendor-patched directories to exclude them from package discovery.
-    $config->packageDirectoriesExcludes(
-        array(
-            __DIR__ . '/components/Blueprints/vendor-patched',
-            __DIR__ . '/components/DataLiberation/vendor-patched',
-        )
-    );
     $config->defaultBranch('trunk');
 	// Release workers - in order to execute.
 	$config->workers(
