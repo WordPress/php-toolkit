82b9d5343682e87fdabd04edda8e0fdaab444474
Adam Zieliński <adam@adamziel.com>
2025-09-07 10:02:05 +0200

    GitHub workflow for package release and split (#174)

---
+++ monorepo-builder.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
+
+return static function ( MBConfig $config ): void {
+	// Where packages live.
+	$config->packageDirectories(
+		array(
+			__DIR__ . '/components',
+		)
+	);
+	$config->defaultBranch( 'trunk' );
+	// Release workers - in order to execute.
+	$config->workers(
+		array(
+			UpdateReplaceReleaseWorker::class,
+			SetCurrentMutualDependenciesReleaseWorker::class,
+			AddTagToChangelogReleaseWorker::class,
+			TagVersionReleaseWorker::class,
+			PushTagReleaseWorker::class,
+			SetNextMutualDependenciesReleaseWorker::class,
+			UpdateBranchAliasReleaseWorker::class,
+			PushNextDevReleaseWorker::class,
+		)
+	);
+};
