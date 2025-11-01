963b38070ce5e20aed2b8f375235adfd0955984f
Adam Zieliński <adam@adamziel.com>
2025-11-01 01:21:27 +0100

    fix wp_scrub_utf8 reference

--- components/DataLiberation/URL/class-cssprocessor.php
+++ components/DataLiberation/URL/class-cssprocessor.php
@@ -3,7 +3,7 @@
 namespace WordPress\DataLiberation\URL;
 
 use function WordPress\Encoding\compat\_wp_scan_utf8;
-use function WordPress\Encoding\compat\_wp_scrub_utf8_fallback;
+use function WordPress\Encoding\wp_scrub_utf8;
 use function WordPress\Encoding\utf8_codepoint_at;
 use function WordPress\Encoding\codepoint_to_utf8_bytes;
 
@@ -1528,7 +1528,7 @@ class CSSProcessor {
 	 */
 	private function decode_string_or_url( int $start, int $length ): string {
 		// Fast path: check if any processing is needed.
-		$slice         = _wp_scrub_utf8_fallback( substr( $this->css, $start, $length ) );
+		$slice         = wp_scrub_utf8( substr( $this->css, $start, $length ) );
 		$special_chars = "\\\r\f\x00";
 		if ( false === strpbrk( $slice, $special_chars ) ) {
 			// No special chars - return raw substring (almost zero allocations).
